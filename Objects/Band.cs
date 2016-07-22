fea45578e335537a8a88ea58e7e5a3ac42324041
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-07-22 07:38:51 -0700

    add GetAll for band

---
+++ Objects/Band.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name
+
+    public Band (string name, int id)
+    {
+      _id = id;
+      _name = name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int bandId = rdr.GetInt32(0);
+        string bandName = rdr.GetString(1);
+        Band newBand = new Band(bandName, bandId);
+        allBands.Add(newBand);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allBands;
+    }
+
+  }
+}
