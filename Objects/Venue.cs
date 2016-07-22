caffcd474f51cef393892e42cc3f58360ffff501
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-07-22 08:46:47 -0700

    add constructor, setters and getters for venue

---
+++ Objects/Venue.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Venue
+  {
+    private int _id;
+    private string _name;
+
+    public Venue(string name, int id = 0)
+    {
+      _id = id;
+      _name = name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+  }
+}
