355fa3a7481846fe495ba0cd8e6c49a036a7bbba
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-07-22 08:49:04 -0700

    add GetAll method for venue

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -29,5 +29,35 @@ namespace BandTracker
     {
       _name = newName;
     }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
   }
 }
